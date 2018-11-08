e9c58b9856bfabc47d13c5489d9465c28d319639
sarahkurd <skurdogh@calpoly.edu>
2018-11-08 00:03:48 -0800

    maps and json

---
+++ app/src/main/java/com/example/lab7_mapsjson/SchoolInfo.kt
@@ -0,0 +1,10 @@
+package com.example.lab7_mapsjson
+
+data class SchoolInfo(
+        val name: String = "",
+        val city: String = "",
+        val state: String = "",
+        val zip: String = "",
+        val latitude: Double = 0.0,
+        val longitude: Double = 0.0
+)
